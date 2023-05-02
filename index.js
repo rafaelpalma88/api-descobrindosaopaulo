f8c5cb6a2afbdc7aeb0425a96bf9ea0b235aa7b8
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-05-02 16:24:58 -0300

    changing indexjs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const server = express()
+server.use(express.json())
+
+let cursos = [
+  { id: '1', name: 'PHP' },
+  { id: '2', name: 'Laravel' }
+]
+
+server.get('/cursos', (req, res) => {
+  return res.json(cursos)
+})
