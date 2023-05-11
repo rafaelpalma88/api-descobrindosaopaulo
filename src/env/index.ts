cc4be37b537a9cfa66ffbd42dc8d1d1cccdb76b1
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-05-11 16:26:44 -0300

    configuring eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,12 +5,12 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.string(),
-  JWT_SECRET: z.string()
+  JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false){
+if (_env.success === false) {
   console.error('Invalid enviroment variables', _env.error.format())
 
   throw new Error('Invalid enviroment variables')
--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,12 @@ app.use(cors())
 
 app.use(router)
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if(err instanceof Error) {
+  if (err instanceof Error) {
     return res.status(400).json({ error: err.message })
   }
   return res.status(500).json({
     status: 'error',
-    message: 'Internal server error'
+    message: 'Internal server error',
   })
 })
 
