fb2bc4af5274252b771a5add3b3f0cc301bd9eca
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-10-06 15:13:25 -0300

    removing environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,8 +4,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3000),
-  DATABASE_URL: z.string(),
-  JWT_SECRET: z.string(),
+  // DATABASE_URL: z.string(),
+  // JWT_SECRET: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
